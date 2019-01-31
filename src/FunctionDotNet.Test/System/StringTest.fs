557a2335e795bf4648edad6f7ea5bc5881ea12f7
Ciaran McCarthy <ciaran.mccarthy@outlook.com>
2019-01-31 14:51:57 +0100

    Added ToUpper, ToLower, ToUpperInvariant and ToLowerInvariant

--- src/FunctionDotNet.Test/System/StringTest.fs
+++ src/FunctionDotNet.Test/System/StringTest.fs
@@ -1,5 +1,6 @@
 module FunctionDotNet.Test.System.StringTest
 
+open System.Globalization
 open NUnit.Framework
 
 [<Test>]
@@ -98,5 +99,71 @@ let ``Substring - length is Some`` () =
     let expectedResult = "ke a sub"
     
     let actualResult = FunctionalDotNet.System.String.substring startIndex length testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+    
+// The example for this test case is taken from:
+// https://docs.microsoft.com/en-us/dotnet/api/system.string.toupper?view=netstandard-2.0#System_String_ToUpper_System_Globalization_CultureInfo_
+[<Test>]
+let ``ToUpper - culture is not specified`` () =
+    let testString = "indigo"
+    let culture = None
+    let expectedResult = "INDIGO"
+    
+    let actualResult = FunctionalDotNet.System.String.toUpper culture testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+    
+// The example for this test case is taken from:
+// https://docs.microsoft.com/en-us/dotnet/api/system.string.toupper?view=netstandard-2.0#System_String_ToUpper_System_Globalization_CultureInfo_
+[<Test>]
+let ``ToUpper - culture is specified`` () =
+    let testString = "indigo"
+    let culture = new CultureInfo("tr-TR",false) |> Some
+    let expectedResult = "İNDİGO"
+    
+    let actualResult = FunctionalDotNet.System.String.toUpper culture testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+    
+[<Test>]
+let ``ToUpperInvariant - returned string matches expected`` () =
+    let testString = "indigo"
+    let expectedResult = "INDIGO"
+    
+    let actualResult = FunctionalDotNet.System.String.toUpperInvariant testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+    
+// The example for this test case is taken from:
+// https://docs.microsoft.com/en-us/dotnet/api/system.string.tolower?view=netstandard-2.0
+[<Test>]
+let ``ToLower - culture is not specified`` () =
+    let testString = "INDIGO"
+    let culture = None
+    let expectedResult = "indigo"
+    
+    let actualResult = FunctionalDotNet.System.String.toLower culture testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+    
+// The example for this test case is taken from:
+// https://docs.microsoft.com/en-us/dotnet/api/system.string.tolower?view=netstandard-2.0
+[<Test>]
+let ``ToLower - culture is specified`` () =
+    let testString = "İNDİGO"
+    let culture = new CultureInfo("tr-TR",false) |> Some
+    let expectedResult = "indigo"
+    
+    let actualResult = FunctionalDotNet.System.String.toLower culture testString
+    
+    Assert.That(actualResult, Is.EqualTo(expectedResult))
+
+[<Test>]
+let ``ToLowerInvariant - returned string matches expected`` () =
+    let testString = "INDIGO"
+    let expectedResult = "indigo"
+    
+    let actualResult = FunctionalDotNet.System.String.toLowerInvariant testString
     
     Assert.That(actualResult, Is.EqualTo(expectedResult))
--- src/FunctionalDotNet/System/String.fs
+++ src/FunctionalDotNet/System/String.fs
@@ -1,6 +1,7 @@
 module FunctionalDotNet.System.String
 
 open System
+open System.Globalization
 
 let trim (trimChars : char array option) (string:String) : string =
     match trimChars with
@@ -24,3 +25,19 @@ let substring (startIndex:int) (length:int option) (string:String) =
     match length with
     | Some length -> string.Substring(startIndex, length)
     | None -> string.Substring(startIndex)
+    
+let toLower (culture:CultureInfo option) (string:String) =
+    match culture with
+    | Some culture -> string.ToLower culture
+    | None -> string.ToLower()
+    
+let toLowerInvariant (string:String) =
+    string.ToLowerInvariant()
+    
+let toUpper (culture:CultureInfo option) (string:String) =
+    match culture with
+    | Some culture -> string.ToUpper culture
+    | None -> string.ToUpper()
+    
+let toUpperInvariant (string:String) =
+    string.ToUpperInvariant()
